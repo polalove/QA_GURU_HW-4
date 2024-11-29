ff48c5123653b3b4c6831ac7792aa775583ce58c
Ivan Novikov <pola2288@yandex.ru>
2024-11-29 20:55:04 +0300

    refactoring code

--- src/test/java/SelenideOpenSoftAssertionsTest.java
+++ src/test/java/SelenideOpenSoftAssertionsTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-;
+
 public class SelenideOpenSoftAssertionsTest {
 
     @BeforeAll
